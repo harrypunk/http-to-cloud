31319360ba888594ca7ff528ed144ed5636e952e
harrypunk <allthatjazz@outlook.com>
2025-04-16 20:45:17 +0800

    init multipart

--- pkg/cos_multipart.go
+++ pkg/cos_multipart.go
@@ -1,8 +1,10 @@
 package feature
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"net/url"
 
@@ -18,7 +20,7 @@ type CosMultiClient struct {
 
 type uploadResult struct {
 	Part int
-	err string
+	Err  error
 }
 
 func (cl *CosMultiClient) Save(ctx context.Context, bucketName, objectKey, fileURL string) error {
@@ -31,15 +33,51 @@ func (cl *CosMultiClient) Save(ctx context.Context, bucketName, objectKey, fileU
 		},
 	})
 
-	httpBuf := HttpBuf{
-		Size: cl.BufSize
+	httpBuf := HttpBuf{Size: cl.BufSize}
+
+	result, _, err := client.Object.InitiateMultipartUpload(ctx, objectKey, nil)
+	if err != nil {
+		return fmt.Errorf("init multipart failed: %v", err)
 	}
+
+	uploadId := result.UploadID
+	log.Printf("upload id: %s", uploadId)
 
 	chunkCh := make(chan []byte)
 	go httpBuf.Get(ctx, fileURL, chunkCh)
-	for chunk := range ch {
-		
+
+	resultCh := make(chan uploadResult)
+	defer close(resultCh)
+
+	totalParts := 0
+	partNum := 1
+	for chunk := range chunkCh {
+		go uploadPart(ctx, client, objectKey, uploadId, partNum, chunk, resultCh)
+		totalParts += 1
+		partNum += 1
 	}
 
+	for range totalParts {
+		rs := <-resultCh
+		log.Printf("part result: %v", rs)
+	}
+
+	resp, _, err := client.Object.CompleteMultipartUpload(ctx, objectKey, uploadId, &cos.CompleteMultipartUploadOptions{})
+	if err != nil {
+		return err
+	}
+
+	log.Printf("complete multipart %v\n", resp)
+
 	return nil
+}
+
+func uploadPart(ctx context.Context, client *cos.Client, objectKey, uploadId string, part int, chunk []byte, resultChan chan<- uploadResult) {
+	resp, err := client.Object.UploadPart(ctx, objectKey, uploadId, part, bytes.NewReader(chunk), nil)
+	if err != nil {
+		resultChan <- uploadResult{Part: part, Err: err}
+	}
+	partETag := resp.Header.Get("ETag")
+	log.Printf("part %d etag: %s", part, partETag)
+	resultChan <- uploadResult{Part: part, Err: nil}
 }
